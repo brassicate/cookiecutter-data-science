effd361c89b93d74af97ab68eb2dffea626f4bd6
Ciaran Judge <ciaran.judge@welfare.ie>
2021-04-20 15:19:13 +0100

    Commit changes to standardise for DSP

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,11 @@
+import subprocess
+
+
+# *Initialise git repo for newly created project
+subprocess.call(['git', 'init'])
+subprocess.call(['git', 'add', '*'])
+subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+
+# # * Create new conda environment
+# # TODO Check if it exists already!
+# subprocess.call(["conda", "env", "create", "-f", "environment.yml"])
--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
-    packages=find_packages(),
+    name='{{ cookiecutter.repo_name }}',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     version='0.1.0',
     description='{{ cookiecutter.description }}',
-    author='{{ cookiecutter.author_name }}',
-    license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
+    author='Department of Social Protection',
+    license='EUPL',
 )
