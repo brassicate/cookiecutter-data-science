d19e6eda8306169ef6628612e9ab0d8b301f9854
Ciaran Judge <ciaran.judge@welfare.ie>
2021-09-30 13:24:43 +0100

    Tidy after fixing click option for config_path

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.repo_name }}/__main__.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.repo_name }}/__main__.py
@@ -8,18 +8,16 @@ from box import Box
 # Inspired by https://medium.com/bcggamma/welcome-to-the-big-leagues-b9038648054f
 # and also by https://medium.com/bcggamma/data-science-python-best-practices-fdb16fdedf82
 
+
 @click.command()
 @click.option("--config_path", default="./config.yml")
 def run(config_path):
     # Read in yaml file
-    with open(Path(config_path), "r") as ymlfile:  
+    with open(Path(config_path), "r") as ymlfile:
         # Use Box to allow dot notation for config
-        config = Box(
-            yaml.safe_load(ymlfile)
-        )  
-    print(
-        f"Hello {config.project_name}!"
-    )  # Check config is working using friendly message!
+        config = Box(yaml.safe_load(ymlfile))
+    # Check config is working using friendly message
+    print(f"Hello {config.project_name}!")
 
 
 if __name__ == "__main__":
