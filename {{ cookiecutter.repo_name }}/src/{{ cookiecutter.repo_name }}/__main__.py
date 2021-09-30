9032c142a8e97099c6b0b596910860b1cbd80807
Ciaran Judge <ciaran.judge@welfare.ie>
2021-09-30 16:01:24 +0100

    Add comments about data and config file locations

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.repo_name }}/__main__.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.repo_name }}/__main__.py
@@ -8,16 +8,21 @@ from box import Box
 # Inspired by https://medium.com/bcggamma/welcome-to-the-big-leagues-b9038648054f
 # and also by https://medium.com/bcggamma/data-science-python-best-practices-fdb16fdedf82
 
+# *Config file(s) should live in the project repo, but outside of the src/ structure.
+
 
 @click.command()
-@click.option("--config_path", default="./config.yml")
+@click.option("-cfg", "--config_path", default="./config.yml")
 def run(config_path):
     # Read in yaml file
-    with open(Path(config_path), "r") as ymlfile:
+    with open(Path(config_path), "r") as ymlfile:  
         # Use Box to allow dot notation for config
-        config = Box(yaml.safe_load(ymlfile))
-    # Check config is working using friendly message
-    print(f"Hello {config.project_name}!")
+        config = Box(
+            yaml.safe_load(ymlfile)
+        )  
+    print(
+        f"Hello {config.project_name}!"
+    )  # Check config is working using friendly message!
 
 
 if __name__ == "__main__":
